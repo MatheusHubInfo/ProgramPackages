3def54bb01c9a1395a27d95a28130367d0384866
MatheusHubInfo <mm5426875@gmail.com>
2022-06-22 19:37:29 -0300

    sem esperança

--- src/entites/Principal.java
+++ src/entites/Principal.java
@@ -5,6 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println("HELLO WORD");
+		System.out.println("HELLO WORD");
+		System.out.println("HELLO WORD");
+		System.out.println("HELLO WORD");
 	}
 
 }
